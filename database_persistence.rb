8391a6db802b89b5327df4050dc98213128088c2
Chris Shieh <dev@chrisgrace.com>
2023-02-24 02:32:49 -0800

    needed this ruby version

--- database_persistence.rb
+++ database_persistence.rb
@@ -2,7 +2,11 @@ require "pg"
 
 class DatabasePersistence
   def initialize(logger)
-    @db = PG.connect(dbname: "185_todo")
+    @db = if Sinatra::Base.production?
+      PG.connect(ENV['DATABASE_URL'])
+    else
+      PG.connect(dbname: "185_todo")
+    end
     @logger = logger
   end
 
